6b5fa027e9954ed4b34116cb6f0a9c4a1e5e3346
Debalina Maity <151049768+Debalina-4128@users.noreply.github.com>
2025-06-29 21:43:45 +0530

    Add files via upload

---
+++ Week2_AdvancedSQL_&_NUnitAndMoq/NUnit&Moq/NUnit/AccountManagerLib/Codes/AccountManagerTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using AccountsManagerLib;
+using System;
+
+namespace AccountsManagerLib.Tests
+{
+    [TestFixture]
+    public class AccountsManagerTests
+    {
+        private AccountsManager manager;
+
+        [SetUp]
+        public void Setup()
+        {
+            manager = new AccountsManager();
+        }
+
+        [Test]
+        public void ValidateUser_ValidUser11_ReturnsWelcomeMessage()
+        {
+            var result = manager.ValidateUser("user_11", "secret@user11");
+            Assert.That(result, Is.EqualTo("Welcome user_11!!!"));
+        }
+
+        [Test]
+        public void ValidateUser_ValidUser22_ReturnsWelcomeMessage()
+        {
+            var result = manager.ValidateUser("user_22", "secret@user22");
+            Assert.That(result, Is.EqualTo("Welcome user_22!!!"));
+        }
+
+        [Test]
+        public void ValidateUser_InvalidCredentials_ReturnsInvalidMessage()
+        {
+            var result = manager.ValidateUser("user_11", "wrongpass");
+            Assert.That(result, Is.EqualTo("Invalid user id/password"));
+        }
+
+        [Test]
+        public void ValidateUser_EmptyUserId_ThrowsFormatException()
+        {
+            var ex = Assert.Throws<FormatException>(() => manager.ValidateUser("", "secret@user11"));
+            Assert.That(ex.Message, Is.EqualTo("Both user id and password are mandatory"));
+        }
+
+        [Test]
+        public void ValidateUser_EmptyPassword_ThrowsFormatException()
+        {
+            var ex = Assert.Throws<FormatException>(() => manager.ValidateUser("user_11", ""));
+            Assert.That(ex.Message, Is.EqualTo("Both user id and password are mandatory"));
+        }
+
+        [Test]
+        public void ValidateUser_NullCredentials_ThrowsFormatException()
+        {
+            var ex = Assert.Throws<FormatException>(() => manager.ValidateUser(null, null));
+            Assert.That(ex.Message, Is.EqualTo("Both user id and password are mandatory"));
+        }
+    }
+}
---
+++ Week2_AdvancedSQL_&_NUnitAndMoq/NUnit&Moq/NUnit/CalcLibrary/Codes/UnitTest2.cs
@@ -0,0 +1,82 @@
+﻿using CalcLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CalcLibrary2.Tests
+{
+    [TestClass]
+    public class SimpleCalculatorTests
+    {
+        private SimpleCalculator calculator;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            calculator = new SimpleCalculator();
+        }
+
+        [TestCleanup]
+        public void Teardown()
+        {
+            calculator.AllClear();
+        }
+
+        [DataTestMethod]
+        [DataRow(5, 3, 2)]
+        [DataRow(3, 5, -2)]
+        [DataRow(-4, -2, -2)]
+        [DataRow(0, 0, 0)]
+        public void Subtraction_ShouldReturnCorrectResult(double a, double b, double expected)
+        {
+            var result = calculator.Subtraction(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(2, 3, 6)]
+        [DataRow(0, 100, 0)]
+        [DataRow(-5, 2, -10)]
+        [DataRow(-3, -4, 12)]
+        public void Multiplication_ShouldReturnCorrectResult(double a, double b, double expected)
+        {
+            var result = calculator.Multiplication(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        [DataTestMethod]
+        [DataRow(10, 2, 5)]
+        [DataRow(7.5, 2.5, 3)]
+        [DataRow(-9, 3, -3)]
+        public void Division_ShouldReturnCorrectResult(double a, double b, double expected)
+        {
+            var result = calculator.Division(a, b);
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Division_ByZero_ShouldThrowException()
+        {
+            try
+            {
+                calculator.Division(10, 0);
+                Assert.Fail("Division by zero"); // If no exception, this line will fail the test
+            }
+            catch (ArgumentException ex)
+            {
+                Assert.AreEqual("Second Parameter Can't be Zero", ex.Message);
+            }
+        }
+
+        [TestMethod]
+        public void TestAddAndClear()
+        {
+            var result = calculator.Addition(4, 6);
+            Assert.AreEqual(10, result);
+            Assert.AreEqual(10, calculator.GetResult);
+
+            calculator.AllClear();
+            Assert.AreEqual(0, calculator.GetResult);
+        }
+    }
+}
+
