c9d86aff65ac74420c52f3f0bbff661c70987c13
Debalina Maity <151049768+Debalina-4128@users.noreply.github.com>
2025-07-20 09:26:26 +0530

    Add files via upload

---
+++ Week5_Microservices/JWT/Controllers/SecureController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthDemo.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SecureController : ControllerBase
+    {
+        [HttpGet]
+        [Authorize]
+        public IActionResult GetSecret()
+        {
+            var username = User.Identity?.Name;
+            return Ok($"Hello {username}, this is a protected endpoint!");
+        }
+    }
+}
